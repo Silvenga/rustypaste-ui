74845017740230a5106b9e5394f4cb189e123f91
Mark Lopez <m@silvenga.com>
2026-07-18 20:13:33 -0500

    style: disabled user-selection by default

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ export function App() {
   const { appVersion, serverVersion } = useVersions();
   const { clearAuth, isAuthenticated } = useAuth();
   return (
-    <div className="container mx-auto flex h-screen w-full flex-col items-center gap-4 p-4">
+    <div className="container mx-auto flex h-screen w-full flex-col items-center gap-4 p-4 select-none">
       <section className="mt-8 flex justify-center">
         <img src={logo} alt="Rustypaste Logo" className="h-16 w-auto" />
       </section>
