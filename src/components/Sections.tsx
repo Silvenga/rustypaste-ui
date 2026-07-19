17c2f0ff93b8f0206f01f83ee5f4f0a3376eb0fd
Mark Lopez <m@silvenga.com>
2026-07-18 20:52:30 -0500

    feat: added hash based routing for the tabs

--- src/components/Sections.tsx
+++ src/components/Sections.tsx
@@ -2,24 +2,27 @@ import { HistorySection } from "@/components/sections/history/HistorySection.tsx
 import { ShortenUrlSection } from "@/components/sections/shorten-url/ShortenUrlSection.tsx";
 import { UploadFileSection } from "@/components/sections/upload-file/UploadFileSection.tsx";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { TabNames, useHashRoute } from "@/components/use-hash-route";
 
 export function Sections() {
+  const [tab, setTab] = useHashRoute();
+
   return (
-    <Tabs defaultValue="files" className="mt-4 flex w-full flex-col items-center">
+    <Tabs value={tab} onValueChange={setTab} className="mt-4 flex w-full flex-col items-center">
       <TabsList className="w-full max-w-md">
-        <TabsTrigger value="files">Files</TabsTrigger>
-        <TabsTrigger value="url" className="hidden">
+        <TabsTrigger value={TabNames.files}>Files</TabsTrigger>
+        <TabsTrigger value={TabNames.url} className="hidden">
           URL
         </TabsTrigger>
-        <TabsTrigger value="history">History</TabsTrigger>
+        <TabsTrigger value={TabNames.history}>History</TabsTrigger>
       </TabsList>
-      <TabsContent value="files" className="w-full max-w-md">
+      <TabsContent value={TabNames.files} className="w-full max-w-md">
         <UploadFileSection />
       </TabsContent>
-      <TabsContent value="url" className="w-full max-w-md">
+      <TabsContent value={TabNames.url} className="w-full max-w-md">
         <ShortenUrlSection />
       </TabsContent>
-      <TabsContent value="history" className="w-full max-w-4xl">
+      <TabsContent value={TabNames.history} className="w-full max-w-4xl">
         <HistorySection />
       </TabsContent>
     </Tabs>
